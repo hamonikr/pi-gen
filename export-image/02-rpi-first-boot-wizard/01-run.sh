f9390614492228c49faa6a83c8122a71acc6361b
배동현 <bdh1993@invesume.com>
2023-04-14 15:16:25 +0900

    .zshrc 권한 변경, 기본 프로그램 변경, 패키지 추가 및 변경

--- export-image/02-rpi-first-boot-wizard/01-run.sh
+++ export-image/02-rpi-first-boot-wizard/01-run.sh
@@ -1,11 +1,33 @@
 #!/bin/bash -e
 
+OWNER=$(cat ${ROOTFS_DIR}/etc/passwd | grep rpi-first-boot-wizard | cut -d':' -f3)
+GROUP=$(cat ${ROOTFS_DIR}/etc/passwd | grep rpi-first-boot-wizard | cut -d':' -f4)
+
+echo $OWNER
+echo $GROUP
+
+# first boot user is rpi-first-boot-wizard
 if [ -d "${ROOTFS_DIR}/home/rpi-first-boot-wizard" ]; then
 	#rpi-first-boot-wizard
-	install -m 644 files/.zshrc "${ROOTFS_DIR}/home/rpi-first-boot-wizard/"
-	sed -i 's#/bin/bash#/usr/bin/zsh#g' ${ROOTFS_DIR}/etc/passwd
+	install -m 644 -v -o $OWNER -g $GROUP files/.zshrc "${ROOTFS_DIR}/home/rpi-first-boot-wizard/"
+	install -m 644 files/.zshrc "${ROOTFS_DIR}/etc/skel/"
+	install -v -o $OWNER -g $GROUP -d "${ROOTFS_DIR}/home/rpi-first-boot-wizard/.config"
+	install -m 600 -v -o 1000 -g 1000 files/mimeapps.list "${ROOTFS_DIR}/home/rpi-first-boot-wizard/.config/"
 else
 	mkdir ${ROOTFS_DIR}/home/rpi-first-boot-wizard
-	install -m 644 files/.zshrc "${ROOTFS_DIR}/home/rpi-first-boot-wizard/"
-	sed -i 's#/bin/bash#/usr/bin/zsh#g' ${ROOTFS_DIR}/etc/passwd
+	install -m 644 -v -o $OWNER -g $GROUP files/.zshrc "${ROOTFS_DIR}/home/rpi-first-boot-wizard/"
+	install -m 644 files/.zshrc "${ROOTFS_DIR}/etc/skel/"
+	install -v -o $OWNER -g $GROUP -d "${ROOTFS_DIR}/home/rpi-first-boot-wizard/.config"
+	install -m 600 -v -o 1000 -g 1000 files/mimeapps.list "${ROOTFS_DIR}/home/rpi-first-boot-wizard/.config/"
 fi
+
+#hamonikr-zsh fixes (pi user)
+install -m 644 -v -o 1000 -g 1000 files/.zshrc "${ROOTFS_DIR}/home/pi/"
+sed -i 's#/bin/bash#/usr/bin/zsh#g' ${ROOTFS_DIR}/etc/passwd
+
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.config"
+install -m 600 -v -o 1000 -g 1000 files/mimeapps.list "${ROOTFS_DIR}/home/pi/.config/"
+
+on_chroot << EOF
+	SUDO_USER="${FIRST_USER_NAME}" systemctl enable NetworkManager.service
+EOF
--- export-image/02-rpi-first-boot-wizard/files/.zshrc
+++ export-image/02-rpi-first-boot-wizard/files/.zshrc
@@ -42,3 +42,9 @@ alias left='ls -t -1'
 alias count='find . -type f | wc -l'
 alias cpv='rsync -ah --info=progress2'
 
+if [ "$(id -u)" -eq 0 ]; then
+    PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+else
+    PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games"
+fi
+export PATH
