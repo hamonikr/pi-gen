d311af32aed8b780e1844db1530bd15824ea7858
김형채 <hckim@invesume.com>
2024-07-16 16:00:26 +0900

    Update debconf for Korean

--- export-image/01-user-rename/01-run.sh
+++ export-image/01-user-rename/01-run.sh
@@ -3,6 +3,9 @@
 if [[ "${DISABLE_FIRST_BOOT_USER_RENAME}" == "0" ]]; then
 	on_chroot <<- EOF
 		SUDO_USER="${FIRST_USER_NAME}" rename-user -f -s
+
+		sed -i 's/autologin-user=.*$/autologin-user=${FIRST_USER_NAME}/g' /etc/lightdm/lightdm.conf || true
+
 	EOF
 else
 	rm -f "${ROOTFS_DIR}/etc/xdg/autostart/piwiz.desktop"
