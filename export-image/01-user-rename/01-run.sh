f9320c28eed8a6db6275bbc0ba4b6c36b5cba38b
김형채 <hckim@invesume.com>
2024-07-16 17:00:42 +0900

    Update Auto login settings

--- export-image/01-user-rename/01-run.sh
+++ export-image/01-user-rename/01-run.sh
@@ -4,6 +4,8 @@ if [[ "${DISABLE_FIRST_BOOT_USER_RENAME}" == "0" ]]; then
 	on_chroot <<- EOF
 		SUDO_USER="${FIRST_USER_NAME}" rename-user -f -s
 
+		echo "Update Auto login settings..."
+		SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_behaviour B4
 		sed -i 's/autologin-user=.*$/autologin-user=${FIRST_USER_NAME}/g' /etc/lightdm/lightdm.conf || true
 
 	EOF
